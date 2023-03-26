6adea5e724244938ca8c46e143523539de15b647
Oskar H <assawin.h@gmail.com>
2023-03-26 14:36:04 +0200

    bogosort

---
+++ bogosort.py
@@ -0,0 +1,16 @@
+import random
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array2 = [3, 2, 5, 7, 4, 6, 9, 1, 10, 8]
+count = 0
+
+while array != array2:
+    random.shuffle(array2)
+    count +=1
+
+
+print(array2)
+print(array)
+print(count)
+
+
